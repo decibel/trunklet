75c32122abd0c4fc1bebf899acc589770927014e
Jim Nasby <Jim.Nasby@BlueTreble.com>
2015-04-24 23:24:30 -0500

    Refactor psql option setting

--- test/helpers/tap_setup.sql
+++ test/helpers/tap_setup.sql
@@ -1,12 +1,4 @@
--- No status messages
-\set QUIET true
-
--- Verbose error messages
-\set VERBOSITY verbose
-
--- Revert all changes on failure.
-\set ON_ERROR_ROLLBACK 1
-\set ON_ERROR_STOP true
+\i test/helpers/psql.sql
 
 CREATE SCHEMA tap;
 SET search_path = tap;
