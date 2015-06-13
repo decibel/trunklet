29e40370fa97b860d2a8d14689c7229f6f14bea8
Jim Nasby <Jim.Nasby@BlueTreble.com>
2015-06-12 19:41:42 -0500

    Switch to using pgTap as an extension

--- test/helpers/tap_setup.sql
+++ test/helpers/tap_setup.sql
@@ -1,9 +1,9 @@
 \i test/helpers/psql.sql
 
 CREATE SCHEMA tap;
+GRANT USAGE ON SCHEMA tap TO public;
 SET search_path = tap;
-\i test/helpers/pgtap-core.sql
-\i test/helpers/pgtap-schema.sql
+CREATE EXTENSION IF NOT EXISTS pgtap SCHEMA tap;
 
 \pset format unaligned
 \pset tuples_only true
