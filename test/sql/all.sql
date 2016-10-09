baab14231ee7527bb91d09df31b155748ef3e861
Jim Nasby <Jim.Nasby@BlueTreble.com>
2016-10-09 16:31:05 -0500

    Add colation test

--- test/sql/all.sql
+++ test/sql/all.sql
@@ -1,6 +1,20 @@
 \set ECHO none
 \i test/helpers/setup.sql
 
+SHOW lc_collate;
+ SELECT DISTINCT quote_ident(n.nspname) || '.' || quote_ident(p.proname) COLLATE "C" AS pname
+          FROM pg_catalog.pg_proc p
+          JOIN pg_catalog.pg_namespace n ON p.pronamespace = n.oid
+         WHERE nspname = '_trunklet_test'
+            AND proname ~ '^test_'
+        ORDER BY pname;
+ SELECT DISTINCT quote_ident(n.nspname) || '.' || quote_ident(p.proname)  AS pname
+          FROM pg_catalog.pg_proc p
+          JOIN pg_catalog.pg_namespace n ON p.pronamespace = n.oid
+         WHERE nspname = '_trunklet_test'
+            AND proname ~ '^test_'
+        ORDER BY pname;
+
 /*
 SET client_min_messages = debug;
 SELECT no_plan();
@@ -10,5 +24,4 @@ SELECT * FROM _trunklet_test.test_process();
 -- Needed for now due to bug in pgtap-core.sql
 SET client_min_messages = WARNING;
 
-SHOW lc_collate;
 SELECT * FROM runtests( '_trunklet_test'::name );
