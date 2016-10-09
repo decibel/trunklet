f871a9e9ffa685b13728ed43af5db49316a856f2
Jim Nasby <Jim.Nasby@BlueTreble.com>
2016-10-09 17:03:26 -0500

    Back out test collation stuff

--- test/sql/all.sql
+++ test/sql/all.sql
@@ -1,20 +1,6 @@
 \set ECHO none
 \i test/helpers/setup.sql
 
-SHOW lc_collate;
- SELECT DISTINCT quote_ident(n.nspname) || '.' || quote_ident(p.proname) COLLATE "C" AS pname
-          FROM pg_catalog.pg_proc p
-          JOIN pg_catalog.pg_namespace n ON p.pronamespace = n.oid
-         WHERE nspname = '_trunklet_test'
-            AND proname ~ '^test_'
-        ORDER BY pname;
- SELECT DISTINCT quote_ident(n.nspname) || '.' || quote_ident(p.proname)  AS pname
-          FROM pg_catalog.pg_proc p
-          JOIN pg_catalog.pg_namespace n ON p.pronamespace = n.oid
-         WHERE nspname = '_trunklet_test'
-            AND proname ~ '^test_'
-        ORDER BY pname;
-
 /*
 SET client_min_messages = debug;
 SELECT no_plan();
